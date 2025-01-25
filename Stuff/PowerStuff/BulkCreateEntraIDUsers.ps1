5031b27e82322cc4ff5c614446fbb5dfb03e5412
Elli Shlomo <eshlomo9@users.noreply.github.com>
2025-01-25 18:55:08 +0200

    Update BulkCreateEntraIDUsers.ps1

--- Stuff/PowerStuff/BulkCreateEntraIDUsers.ps1
+++ Stuff/PowerStuff/BulkCreateEntraIDUsers.ps1
@@ -1,32 +1,33 @@
-# BulkCreateEntraIDUsers.ps1
-# Title: Bulk create users in Microsoft Graph using PowerShell
+# BulkCreateUniqueEntraIDUsers.ps1
+# Title: Bulk Create Unique Users in Azure AD with Microsoft Graph
+# Description: This PowerShell script automates the creation of multiple Entra ID 
+#   users using the Microsoft Graph API. It ensures unique UserPrincipalName and MailNickname 
+#   values by appending randomized suffixes. The script enforces secure password policies, requiring
 
 # ------------------------------------------------------------------
-
-# Import the Microsoft Graph Users module
-Import-Module Microsoft.Graph.Users
-
-# Connect to Microsoft Graph with the required permission
-Connect-MgGraph -Scope User.ReadWrite.All
-
-# Define the password profile settings
-$PasswordProfile = @{
-    Password = "SecurePass123!"
-    ForceChangePasswordNextSignIn = $true
-}
 
 # Bulk create 10 users using a loop
 1..10 | ForEach-Object {
     $Index = $_
+
+    # Define the password profile settings
+    $PassProfile = @{
+        Password = "SecPass789!@#"
+        ForceChangePasswordNextSignIn = $true
+    }
+
+    # Generate a random suffix for uniqueness
+    $RandomSuffix = Get-Random -Minimum 1000 -Maximum 9999
+
+    # Define user parameters
     $UserParams = @{
         DisplayName       = "User$Index"
-        UserPrincipalName = "user$Index@your_domain.com"
-        MailNickname      = "user$Index"
+        UserPrincipalName = "user$Index$RandomSuffix@domain.com"
+        MailNickname      = "user$Index$RandomSuffix"
         AccountEnabled    = $true
-        PasswordProfile   = @{
-            Password = "Your_Password"
-            ForceChangePasswordNextSignIn = $true
-        }
+        PasswordProfile   = $PassProfile
     }
+
+    # Create the user
     New-MgUser -BodyParameter $UserParams
 }
